9886ca8c7add880dd3dbd8c8605b9cea025be94f
Colin Higgins <DeveloperSPike@gmail.com>
2014-03-14 15:57:31 -0400

    Setting up your entity for you. ;]

--- SharpNote/SharpNote.Data/Entities/BlogEntry.cs
+++ SharpNote/SharpNote.Data/Entities/BlogEntry.cs
@@ -1,12 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassLibrary1.Entities
 {
     class BlogEntry
     {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Author Author { get; set; }
+        public ICollection<Comment> Comments { get; set; }
     }
 }
